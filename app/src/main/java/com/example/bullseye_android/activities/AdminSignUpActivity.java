0f677c753dfd5f1904802d63896b49962d6e1b22
xaridar <xaridara@gmail.com>
2020-09-21 13:01:39 -0400

    add admin sign up activity

---
+++ app/src/main/java/com/example/bullseye_android/activities/AdminSignUpActivity.java
@@ -0,0 +1,16 @@
+package com.example.bullseye_android.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminSignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
