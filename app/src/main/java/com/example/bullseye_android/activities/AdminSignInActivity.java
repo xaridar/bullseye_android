7f868b78be410986b1194df7ff23e1c6ca038b9d
aakashsell <aakashsell@gmail.com>
2020-09-25 12:44:46 -0400

    home screen

---
+++ app/src/main/java/com/example/bullseye_android/activities/AdminSignInActivity.java
@@ -0,0 +1,16 @@
+package com.example.bullseye_android.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bullseye_android.R;
+
+public class AdminSignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_sign_in);
+    }
+}
