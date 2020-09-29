0c2a802f5e909ad62a48e3b1608161b18cd973ff
aakashsell <aakashsell@gmail.com>
2020-09-28 21:44:23 -0400

    changes before bed

--- app/src/main/java/com/example/bullseye_android/activities/UsersActivity.java
+++ app/src/main/java/com/example/bullseye_android/activities/UsersActivity.java
@@ -2,7 +2,11 @@ package com.example.bullseye_android.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.bullseye_android.R;
 
@@ -12,5 +16,53 @@ public class UsersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
+        run();
+
+    }
+
+    public void run(){
+        ImageButton button = findViewById(R.id.addUser);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), UserSignUpActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+        ImageButton button2 = findViewById(R.id.pfp1);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), AdminSignInActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+        ImageButton button3 = findViewById(R.id.pfp2);
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), AdminSignInActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+        ImageButton button4 = findViewById(R.id.pfp3);
+        button4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), AdminSignInActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+        ImageButton button5 = findViewById(R.id.pfp4);
+        button5.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), AdminSignInActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+        Button button6 = findViewById(R.id.adminSignIn);
+        button6.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), AdminSignInActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
     }
 }
