7c69fd6d03a8e1f54e042e1059c37a881077d978
aakashsell <aakashsell@gmail.com>
2020-09-29 20:11:35 -0400

    i have no life

--- app/src/main/java/com/example/bullseye_android/activities/UserSignUpActivity.java
+++ app/src/main/java/com/example/bullseye_android/activities/UserSignUpActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 import com.example.bullseye_android.R;
+import com.example.bullseye_android.database.User;
+
+import java.util.function.Function;
 
 public class UserSignUpActivity extends AppCompatActivity {
     Button btn;
@@ -14,13 +18,25 @@ public class UserSignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_sign_up);
+        Function<User, Void> adminCallback = user -> {
+            if (user != null) {
+                Intent intent = new Intent(UserSignUpActivity.this, UsersActivity.class);
+                intent.putExtra("sender", "userSignUp");
+                Log.i(getPackageName(), "user exists");
+                startActivity(intent);
+                finish();
+            } else {
+                run();
+            }
+            return null;
+        };
     }
 
     public void run(){
         btn = findViewById(R.id.btn2);
         btn.setOnClickListener((view) -> {
 
-            startActivity(new Intent(UserSignUpActivity.this, UsersActivity.class));
+            startActivity(new Intent(UserSignUpActivity.this, TransitionActivity.class));
         });
 
     }
