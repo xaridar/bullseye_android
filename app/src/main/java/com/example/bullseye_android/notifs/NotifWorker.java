c6f9c82d8494f78ecd3b35a1d5a96fb85854143e
aakashsell <aakashsell@gmail.com>
2020-11-09 16:42:52 -0500

    notif-worker

--- app/src/main/java/com/example/bullseye_android/notifs/NotifWorker.java
+++ app/src/main/java/com/example/bullseye_android/notifs/NotifWorker.java
@@ -4,24 +4,27 @@ import android.content.Context;
 
 import androidx.annotation.NonNull;
 import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
+
+import com.example.bullseye_android.activities.UsersActivity;
 
 import java.util.concurrent.TimeUnit;
 
 public class NotifWorker extends Worker {
 
-    PeriodicWorkRequest saveRequest =
-            new PeriodicWorkRequest.Builder(this.getClass(), 1, TimeUnit.HOURS)
-                    // Constraints
-                    .build();
+    Notify notification;
+
     public NotifWorker(@NonNull Context context, @NonNull WorkerParameters params) {
         super(context, params);
+
     }
 
     @Override
     public Result doWork() {
-
+        notification = new Notify(getApplicationContext());
+        notification.createNotification(getApplicationContext(), "Where have you been??", "We have missed you. Come play some games with bullseye!!", UsersActivity.class);
         return Result.success();
     }
 }
